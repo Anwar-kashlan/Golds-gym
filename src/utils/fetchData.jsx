98319ce26e703acb1d5eedcadc97ff9d675e2d37
anwar kashlan <anwar98.kn@gmail.com>
2023-08-03 20:21:10 +0300

    diploy website

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,14 +1,14 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
 export const videoOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API,
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
